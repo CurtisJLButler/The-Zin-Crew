338f8b35b147e430de42e499366c688c6396202c
W0468710 <w0468710@nscc.ca>
2025-03-03 11:56:44 -0400

    Added the design file to the homepage for now

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,26 +8,38 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div class="designColor dark-white">
+        #f7f8fa <br></br>
+        dark-white
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div class="designColor white">
+        #f6fbf7 <br></br>
+        white
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <div class="designColor light-blue">
+        #5e8bb5 <br></br>
+        light-blue
+      </div>
+      <div class="designColor blue">
+        #1d3d64 <br></br>
+        blue
+        </div>
+      <div class="designColor dark-blue">
+        #1d3d64 <br></br>
+        dark-blue
+      </div>
+      <div class="designColor dark-grey">
+        #66696e <br></br>
+        dark-grey
+      </div>
+      <div class="designColor grey">
+        #b0b5b9 <br></br>
+        grey
+      </div>
+      <div class="designColor black">
+        #010302 <br></br>
+        black
+        </div>
     </>
   )
 }
