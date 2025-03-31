6d1a4713bb2ad2f45fb449e65f8870ffed4cd4fb
W0468723 <W0468723@nscc.ca>
2025-03-31 20:47:11 -0300

    have front end ready to add more pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,49 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react'
+import NavBar from './components/NavBar'
+import Main from './components/Main';
+import Footer from './components/Footer'
+import { Route, Routes } from 'react-router-dom'
+
+//import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-    <h2>
-      set class to &lt;color&gt; to set the color
-    </h2>
-      <div class="designColor dark-white">
-        #f7f8fa <br></br>
-        dark-white
+    <React.Fragment>
+      <NavBar />
+      <div id="main-content">
+        <Routes>
+          <Route path='/' element={<Main />} />
+        </Routes>
       </div>
-      <div class="designColor white">
-        #f6fbf7 <br></br>
-        white
-      </div>
-      <div class="designColor light-blue">
-        #5e8bb5 <br></br>
-        light-blue
-      </div>
-      <div class="designColor blue">
-        #1d3d64 <br></br>
-        blue
-        </div>
-      <div class="designColor dark-blue">
-        #1d3d64 <br></br>
-        dark-blue
-      </div>
-      <div class="designColor dark-grey">
-        #66696e <br></br>
-        dark-grey
-      </div>
-      <div class="designColor grey">
-        #b0b5b9 <br></br>
-        grey
-      </div>
-      <div class="designColor black">
-        #010302 <br></br>
-        black
-        </div>
-    </>
+      <Footer />
+    </React.Fragment>
   )
 }
 
---
+++ client/src/components/NavBar.jsx
@@ -0,0 +1,34 @@
+import { Link, useNavigate } from 'react-router-dom';
+import React from 'react';
+
+const NavBar = () => {
+    return (
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+      <div className="container">
+        <a href="/#" className="navbar-brand d-flex align-items-center">
+          <strong>Cars</strong>
+        </a>
+       
+
+        <div className="collapse navbar-collapse" id="navbarsExample07">
+          <ul className="navbar-nav mr-auto">
+          <li className="nav-item">
+            <Link className="nav-link" to="/">Home</Link>
+            </li>
+            <li className="nav-item">
+            <Link className="nav-link" to="/">Inventory</Link>
+            </li>
+            <li className="nav-item">
+            <Link className="nav-link" to="/">About us</Link>
+            </li>
+            <li className="nav-item">
+            <Link className="nav-link" to="/">Contact</Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+    );
+}
+ 
+export default NavBar;
