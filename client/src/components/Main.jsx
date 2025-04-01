1fd82b076d16c4d36e0618354aefc2a7c3da567b
W0468723 <W0468723@nscc.ca>
2025-04-01 10:08:41 -0300

    little

--- client/src/components/Main.jsx
+++ client/src/components/Main.jsx
@@ -5,6 +5,7 @@ import React from 'react';
 const Main = () => {
     return (
         <div>
+            <img src="https://cf.cdn.uplynk.com/ausw/slices/f9d/be88c4e651db4a7dbe102614d7272948/f9d60a2849194b4fab396677faaee768/poster_34f51998fbd34da2a132f940ea18d2b8.jpeg" width="100%" height="300"></img>
             <p> hello</p>
         </div>
     )
