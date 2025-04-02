09d955fe509c8bb8b5c52d25e4dcc8797532dfea
W0468723 <W0468723@nscc.ca>
2025-04-02 09:59:01 -0300

    no comit

--- client/src/components/Main.jsx
+++ client/src/components/Main.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import carHome from '../assets/carHome.jpeg'
 //import '../css/main.css'
 //import 'font-awesome/css/font-awesome.min.css';
 
 const Main = () => {
     return (
         <div>
+            <img src={carHome} style={{width: "100%", height: "300px"}} />
             <p> hello</p>
         </div>
     )
